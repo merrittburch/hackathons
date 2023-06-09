cd00288b77ace105a0b6b5424aa50ad88610e299
Merritt Burch <mbb262@cbsumm33.biohpc.cornell.edu>
2023-06-09 13:44:28 -0400

    ridge regression and lasso

--- 2023_06_teML/src/06_ridge_regression_lasso.R
+++ 2023_06_teML/src/06_ridge_regression_lasso.R
@@ -11,41 +11,43 @@
 # https://www.pluralsight.com/guides/linear-lasso-and-ridge-regression-with-r
 # ------------------------------------------------------------------------------
 
-# Set directory to b73 gff file
-setwd('/Users/mbb262-admin/Library/CloudStorage/Box-Box/git_projects/hackathons/2023_06_teML/data/te_nucleotideTransformer_results/')
+# Set directory to files
+setwd('/workdir/mbb262/te/te_nucleotideTransformer_results/')
 
 # Load packages
 library(glmnet)
 library(dplyr)
 library(data.table)
-library(caret)
+# library(caret)
 
 # Load in all chunks of results, rbind them
-te_results <- list.files('/Users/mbb262-admin/Library/CloudStorage/Box-Box/git_projects/hackathons/2023_06_teML/data/te_nucleotideTransformer_results',
-                         pattern = "*.txt", full.names = T)
-te_results_list <- lapply(te_results, data.table::fread)
+te_results <- list.files('/workdir/mbb262/te/te_nucleotideTransformer_results/',
+                         pattern = "*chunk.txt", full.names = T)
+te_results_list <- lapply(te_results, data.table::fread,  nThread= 60)
 results_df <- data.table::rbindlist(te_results_list)
 dim(results_df)
 
 # Load in expression data, drop the sequence
-exp_df <- read.delim(file = 'te_sequence_with_walley_expression.txt') %>% 
+exp_df <- read.delim(file = '../te_sequence_with_walley_expression.txt') %>% 
   select(-"teSeq")
 dim(exp_df)
 
-
 # Merge with expression data
 exp_resuls_df <- cbind(exp_df, results_df)
+dim(exp_resuls_df)
 
-# Hack a thing -----------------------------------------------------------------
-test1 <- data.table::fread('/Users/mbb262-admin/Library/CloudStorage/Box-Box/git_projects/hackathons/2023_06_teML/data/te_nucleotideTransformer_results/01_te_sequence_with_walley_expression.txt_result_output_chunk.txt')
-test_exp_df <- exp_df[1:610,]
-sub_exp_resuls_df <- cbind(test_exp_df, test1)
 
-# Split into training and testing
+# Split into training and testing ----------------------------------------------
+
+# Set seed for consistency
 set.seed(100) 
-index = sample(1:nrow(test1), 0.7*nrow(test1)) 
-train = test1[index,] # Create the training data 
-test = test1[-index,] # Create the test data
+
+# Do a 70/30 training/testing split and sample rows
+index = sample(1:nrow(exp_resuls_df), 0.7*nrow(exp_resuls_df)) 
+train = exp_resuls_df[index,] # Create the training data 
+test = exp_resuls_df[-index,] # Create the test data
+
+# Check if they're subsampled
 dim(train)
 dim(test)
 
@@ -63,7 +65,7 @@ y_test <- test$exp %>% as.matrix()
 lambdas <- 10^seq(2, -3, by = -.1)
 
 # Using glmnet function to build the ridge regression in r
-ridge_reg = glmnet(x, y_train, nlambda = 25, alpha = 0, family = 'gaussian', lambda = lambdas)
+ridge_reg = glmnet(x_train, y_train, nlambda = 25, alpha = 0, family = 'gaussian', lambda = lambdas)
 
 # Checking the model
 summary(ridge_reg)
@@ -73,31 +75,106 @@ cv_ridge <- cv.glmnet(x_train, y_train, alpha = 0, lambda = lambdas)
 optimal_lambda <- cv_ridge$lambda.min
 optimal_lambda
 
-# rebild model with best lambda
-best_fit <- ridge_cv$glmnet.fit
-head(best_fit)
+# Compute R^2 from true and predicted values
+eval_results <- function(true, predicted, df) {
+  SSE <- sum((predicted - true)^2)
+  SST <- sum((true - mean(true))^2)
+  R_square <- 1 - SSE / SST
+  RMSE = sqrt(SSE/nrow(df))
+  
+  # Model performance metrics
+  data.frame(
+    RMSE = RMSE,
+    Rsquare = R_square
+  )
+}
 
-# Rebuilding the model with optimal lambda value
-best_ridge <- glmnet(x, y, alpha = 0, lambda = best_lambda)
+# Prediction on train data --------------
+predictions_train_rr <- predict(ridge_reg, s = optimal_lambda, newx = x_train)
+eval_results(y_train, predictions_train_rr, train)
+plot_train_rr <- data.frame(y_train, predictions_train_rr)
+write.table(plot_train_rr, "../predicted_observed_train_ridge_regression.csv",
+            quote = FALSE, row.names = FALSE)
 
-# here x is the test dataset
-pred <- predict(best_ridge, s = best_lambda, newx = x)
+# Plot predicted vs observed
+plot_value <- paste0("R^2 = ", round(eval_results(y_train, predictions_train_rr, train)[2], 3))
+rr_train_plot <- ggplot(plot_train_rr, aes(x= s1, y = y_train))+
+  geom_point() +
+  geom_label(label = plot_value, x = 1.5, y = 13) +
+  xlab("Predicted") +
+  ylab("Observed") +
+  ggtitle("Train: RR on B73 Mature Leaf ~ All TE Sequence within 10 Kb")
+ggsave("/workdir/mbb262/te/rr_train_plot.png", rr_train_plot, 
+       width = 4, height = 5, units = "in")
 
-# R squared formula
-actual <- test$Price
-preds <- test$PreditedPrice
-rss <- sum((preds - actual) ^ 2)
-tss <- sum((actual - mean(actual)) ^ 2)
-rsq <- 1 - rss/tss
-rsq
+
+# Prediction on test data ----------------
+predictions_test_rr <- predict(ridge_reg, s = optimal_lambda, newx = x_test)
+eval_results(y_test, predictions_test_rr, test)
+plot_test_rr <- data.frame(y_test, predictions_test_rr)
+write.table(plot_test_rr, "../predicted_observed_test_ridge_regression.csv",
+            quote = FALSE, row.names = FALSE)
+
+# Plot test data 
+plot_value <- paste0("R^2 = ", round(eval_results(y_test, predictions_test_rr, test)[2], 3))
+rr_test_plot <- ggplot(plot_test_rr, aes(x= s1, y = y_test))+
+  geom_point() +
+  geom_label(label = plot_value, x = 1.5, y = 13) +
+  xlab("Predicted") +
+  ylab("Observed") +
+  ggtitle("Test: RR on B73 Mature Leaf ~ All TE Sequence within 10 Kb")
+ggsave("/workdir/mbb262/te/rr_test_plot.png", rr_test_plot)
 
 
 # Run lasso --------------------------------------------------------------------
-fit <- glmnet(x, y)
-plot(fit)
-print(fit)
-cvfit <- cv.glmnet(x, y)
-plot(cvfit)
-predict(cvfit, newx = x[1:5,], s = "lambda.min")
+
+# Set range for lambdas
+lambdas <- 10^seq(2, -3, by = -.1)
+
+# Setting alpha = 1 implements lasso regression
+lasso_reg <- cv.glmnet(x_train, y_train, alpha = 1, lambda = lambdas, standardize = TRUE, nfolds = 5)
+
+# Find best lambda 
+lambda_best <- lasso_reg$lambda.min
+plot(lambda_best)
+lambda_best
+
+# Train the lasso model
+lasso_model <- glmnet(x_train, y_train, alpha = 1, lambda = lambda_best, standardize = TRUE)
+
+# Predict on training data ------------
+predictions_train_lasso <- predict(lasso_model, s = lambda_best, newx = x_train)
+eval_results(y_train, predictions_train_lasso, train)
+plot_train_lasso <- data.frame(y_train, predictions_train_lasso)
+write.table(plot_train_lasso, "../predicted_observed_train_lasso.csv",
+            quote = FALSE, row.names = FALSE)
+
+# Plot predicted vs observed training data
+plot_value <- paste0("R^2 = ", round(eval_results(y_train, predictions_train_lasso, train)[2], 3))
+lasso_train_plot <- ggplot(plot_train_lasso, aes(x= s1, y = y_train))+
+  geom_point() +
+  geom_label(label = plot_value, x = 1.5, y = 13) +
+  xlab("Predicted") +
+  ylab("Observed") +
+  ggtitle("Train: Lasso on B73 Mature Leaf ~ All TE Sequence within 10 Kb")
+ggsave("/workdir/mbb262/te/lasso_train_plot.png", lasso_train_plot)
+
+
+# Predict on testing data --------------
+predictions_test_lasso <- predict(lasso_model, s = lambda_best, newx = x_test)
+eval_results(y_test, predictions_test_lasso, test)
+plot_test_lasso <- data.frame(y_test, predictions_test_lasso)
+write.table(plot_test_lasso, "../predicted_observed_test_lasso.csv",
+            quote = FALSE, row.names = FALSE)
+
+# Plot predicted vs observed testing data
+plot_value <- paste0("R^2 = ", round(eval_results(y_test, predictions_test_lasso, test)[2], 3))
+lasso_test_plot <- ggplot(plot_test_lasso, aes(x= s1, y = y_test))+
+  geom_point() +
+  geom_label(label = plot_value, x = 1.5, y = 13) +
+  xlab("Predicted") +
+  ylab("Observed") +
+  ggtitle("Test: Lasso on B73 Mature Leaf ~ All TE Sequence within 10 Kb")
+ggsave("/workdir/mbb262/te/lasso_test_plot.png", lasso_test_plot)
 
 
