e58fa5a88cfd8c423b1bdc810276dbc881991f16
Merritt Khaipho-Burch <mbb262@cornell.edu>
2023-08-29 08:39:16 -0400

    lasso and ridge regression

--- 2023_06_teML/src/06_ridge_regression_lasso.R
+++ 2023_06_teML/src/06_ridge_regression_lasso.R
@@ -12,18 +12,18 @@
 # ------------------------------------------------------------------------------
 
 # Set directory to files
-setwd('/workdir/mbb262/te/te_nucleotideTransformer_results/')
+setwd('/Users/mbb262-admin/Library/CloudStorage/Box-Box/git_projects/hackathons/2023_06_teML/data/te_nucleotideTransformer_results/')
 
 # Load packages
 library(glmnet)
 library(dplyr)
 library(data.table)
-# library(caret)
+library(ggplot2)
 
 # Load in all chunks of results, rbind them
-te_results <- list.files('/workdir/mbb262/te/te_nucleotideTransformer_results/',
+te_results <- list.files('/Users/mbb262-admin/Library/CloudStorage/Box-Box/git_projects/hackathons/2023_06_teML/data/te_nucleotideTransformer_results',
                          pattern = "*chunk.txt", full.names = T)
-te_results_list <- lapply(te_results, data.table::fread,  nThread= 60)
+te_results_list <- lapply(te_results, data.table::fread)
 results_df <- data.table::rbindlist(te_results_list)
 dim(results_df)
 
@@ -31,6 +31,7 @@ dim(results_df)
 exp_df <- read.delim(file = '../te_sequence_with_walley_expression.txt') %>% 
   select(-"teSeq")
 dim(exp_df)
+exp_df <- exp_df[1:181524,]
 
 # Merge with expression data
 exp_resuls_df <- cbind(exp_df, results_df)
@@ -89,41 +90,48 @@ eval_results <- function(true, predicted, df) {
   )
 }
 
-# Prediction on train data --------------
+# Prediction on train data ---------------------------
 predictions_train_rr <- predict(ridge_reg, s = optimal_lambda, newx = x_train)
 eval_results(y_train, predictions_train_rr, train)
 plot_train_rr <- data.frame(y_train, predictions_train_rr)
-write.table(plot_train_rr, "../predicted_observed_train_ridge_regression.csv",
-            quote = FALSE, row.names = FALSE)
+# write.table(plot_train_rr, "../predicted_observed_train_ridge_regression.csv",
+#             quote = FALSE, row.names = FALSE)
+plot_train_rr <- read.csv("../lasso_ridge_regression_results/predicted_observed_train_ridge_regression.csv", header = TRUE, sep = " ")
 
 # Plot predicted vs observed
-plot_value <- paste0("R^2 = ", round(eval_results(y_train, predictions_train_rr, train)[2], 3))
+# plot_value <- paste0("R^2 = ", round(eval_results(y_train, predictions_train_rr, train)[2], 3))
 rr_train_plot <- ggplot(plot_train_rr, aes(x= s1, y = y_train))+
   geom_point() +
-  geom_label(label = plot_value, x = 1.5, y = 13) +
+  # geom_label(label = plot_value, x = 1.5, y = 13) +
   xlab("Predicted") +
   ylab("Observed") +
   ggtitle("Train: RR on B73 Mature Leaf ~ All TE Sequence within 10 Kb")
-ggsave("/workdir/mbb262/te/rr_train_plot.png", rr_train_plot, 
-       width = 4, height = 5, units = "in")
+
+ggsave("../../images/rr_train_plot.png", rr_train_plot, 
+       width = 8, height = 5, units = "in")
 
 
-# Prediction on test data ----------------
+# Prediction on test data ---------------------------
 predictions_test_rr <- predict(ridge_reg, s = optimal_lambda, newx = x_test)
 eval_results(y_test, predictions_test_rr, test)
 plot_test_rr <- data.frame(y_test, predictions_test_rr)
-write.table(plot_test_rr, "../predicted_observed_test_ridge_regression.csv",
-            quote = FALSE, row.names = FALSE)
+# write.table(plot_test_rr, "../predicted_observed_test_ridge_regression.csv",
+#             quote = FALSE, row.names = FALSE)
+plot_test_rr <- read.csv("../lasso_ridge_regression_results/predicted_observed_test_ridge_regression.csv", header = TRUE, sep = " ")
 
 # Plot test data 
-plot_value <- paste0("R^2 = ", round(eval_results(y_test, predictions_test_rr, test)[2], 3))
+# plot_value <- paste0("R^2 = ", round(eval_results(y_test, predictions_test_rr, test)[2], 3))
 rr_test_plot <- ggplot(plot_test_rr, aes(x= s1, y = y_test))+
   geom_point() +
-  geom_label(label = plot_value, x = 1.5, y = 13) +
+  # geom_label(label = plot_value, x = 1.5, y = 13) +
   xlab("Predicted") +
   ylab("Observed") +
   ggtitle("Test: RR on B73 Mature Leaf ~ All TE Sequence within 10 Kb")
-ggsave("/workdir/mbb262/te/rr_test_plot.png", rr_test_plot)
+
+ggsave("../../images/rr_test_plot.png", rr_test_plot, 
+       width = 8, height = 5, units = "in")
+
+
 
 
 # Run lasso --------------------------------------------------------------------
@@ -142,39 +150,45 @@ lambda_best
 # Train the lasso model
 lasso_model <- glmnet(x_train, y_train, alpha = 1, lambda = lambda_best, standardize = TRUE)
 
-# Predict on training data ------------
+# Predict on training data -----------------------------
 predictions_train_lasso <- predict(lasso_model, s = lambda_best, newx = x_train)
 eval_results(y_train, predictions_train_lasso, train)
 plot_train_lasso <- data.frame(y_train, predictions_train_lasso)
-write.table(plot_train_lasso, "../predicted_observed_train_lasso.csv",
-            quote = FALSE, row.names = FALSE)
+# write.table(plot_train_lasso, "../predicted_observed_train_lasso.csv",
+#             quote = FALSE, row.names = FALSE)
+plot_train_lasso <- read.csv("../lasso_ridge_regression_results/predicted_observed_train_lasso.csv", header = TRUE, sep = " ")
 
 # Plot predicted vs observed training data
-plot_value <- paste0("R^2 = ", round(eval_results(y_train, predictions_train_lasso, train)[2], 3))
+# plot_value <- paste0("R^2 = ", round(eval_results(y_train, predictions_train_lasso, train)[2], 3))
 lasso_train_plot <- ggplot(plot_train_lasso, aes(x= s1, y = y_train))+
   geom_point() +
-  geom_label(label = plot_value, x = 1.5, y = 13) +
+  # geom_label(label = plot_value, x = 1.5, y = 13) +
   xlab("Predicted") +
   ylab("Observed") +
   ggtitle("Train: Lasso on B73 Mature Leaf ~ All TE Sequence within 10 Kb")
-ggsave("/workdir/mbb262/te/lasso_train_plot.png", lasso_train_plot)
+
+ggsave("../../images/lasso_train_plot.png", lasso_train_plot, 
+       width = 8, height = 5, units = "in")
 
 
-# Predict on testing data --------------
+# Predict on testing data ------------------------------
 predictions_test_lasso <- predict(lasso_model, s = lambda_best, newx = x_test)
 eval_results(y_test, predictions_test_lasso, test)
 plot_test_lasso <- data.frame(y_test, predictions_test_lasso)
-write.table(plot_test_lasso, "../predicted_observed_test_lasso.csv",
-            quote = FALSE, row.names = FALSE)
+# write.table(plot_test_lasso, "../predicted_observed_test_lasso.csv",
+#             quote = FALSE, row.names = FALSE)
+plot_test_lasso <- read.csv("../lasso_ridge_regression_results/predicted_observed_test_lasso.csv", header = TRUE, sep = " ")
+
 
 # Plot predicted vs observed testing data
-plot_value <- paste0("R^2 = ", round(eval_results(y_test, predictions_test_lasso, test)[2], 3))
+# plot_value <- paste0("R^2 = ", round(eval_results(y_test, predictions_test_lasso, test)[2], 3))
 lasso_test_plot <- ggplot(plot_test_lasso, aes(x= s1, y = y_test))+
   geom_point() +
-  geom_label(label = plot_value, x = 1.5, y = 13) +
+  # geom_label(label = plot_value, x = 1.5, y = 13) +
   xlab("Predicted") +
   ylab("Observed") +
   ggtitle("Test: Lasso on B73 Mature Leaf ~ All TE Sequence within 10 Kb")
-ggsave("/workdir/mbb262/te/lasso_test_plot.png", lasso_test_plot)
+ggsave("../../images/lasso_test_plot.png", lasso_test_plot, 
+       width = 8, height = 5, units = "in")
 
 
